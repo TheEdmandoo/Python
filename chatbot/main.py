9a5b44bac7123e710125c1478f9e9e6f5c56f550
TheEdmandoo <EdwardHart@Edwards-MacBook-Pro.local>
2015-03-05 10:34:04 +0000

    update to file

--- chatbot/main.py
+++ chatbot/main.py
@@ -5,9 +5,30 @@ for word in stopwords:
     next = word.strip()
     print(next)
 
+name = " "    
+
+def binstop(message):
+    for word in stopwords:
+        rubbish = word.strip()
+        message = message.replace( " " + rubbish + " ", " ")
+    message = message.replace( " name " , " ")
+    return message
+
 while True:
-    input = raw_input("Say something interesting: ")
-    filtered = input.replace(" ni ", " <beep> ")
-    print("Your filtered text was: " + filtered)
+
+    input = raw_input("Hello, Whats your name?: ")
+    input = " " + input + " "
+    name = binstop(input)
+    print("hello " + name.strip() + " im chatbot")
+
+    input = raw_input("Whats your mood today " + name + "?")
+    input = " " + input + " "
+    print("Good to hear " + name.strip() + ", I feel the same")
     
+    input = raw_input("Do you ever feel," + name + "like a plastic bag?")
+    input = " " + input + " "
+    print("I see")
     
+    input = raw_input("Do you like Katy Perry," + name + "?")
+    input = " " + input + " "
+    print("I understand, I suppose shes ok.")
